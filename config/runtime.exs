8b0bb66773bc9fef4700e2ab45494340d49b8b85
Adriano Pereira <32802678+adrianorazzor@users.noreply.github.com>
2024-12-03 09:29:09 -0300

    Update runtime.exs

--- config/runtime.exs
+++ config/runtime.exs
@@ -33,15 +33,20 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-   # Configure the Endpoint URL and HTTP settings dynamically
-   host = System.get_env("PHX_HOST") || "razzor.dev"
-   port = String.to_integer(System.get_env("PORT") || "4000")
- 
-   config :razzor_dev, RazzorDevWeb.Endpoint,
-    url: [host: System.get_env("PHX_HOST"), port: 443, scheme: "https"],
+  host = System.get_env("PHX_HOST") || "example.com"
+  port = String.to_integer(System.get_env("PORT") || "4000")
+
+  config :razzor_dev, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
+
+  config :razzor_dev, RazzorDevWeb.Endpoint,
+    url: [host: host, port: 443, scheme: "https"],
     http: [
+      # Enable IPv6 and bind on all interfaces.
+      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
+      # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
+      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: String.to_integer(System.get_env("PORT") || "4000")
+      port: port
     ],
     secret_key_base: secret_key_base
 
@@ -94,4 +99,4 @@ if config_env() == :prod do
   #     config :swoosh, :api_client, Swoosh.ApiClient.Hackney
   #
   # See https://hexdocs.pm/swoosh/Swoosh.html#module-installation for details.
-  end
+end

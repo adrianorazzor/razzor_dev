3a8b17f49ac03de4271734a1e62d3e5ee3796c2a
Adriano Pereira <32802678+adrianorazzor@users.noreply.github.com>
2024-12-03 10:02:44 -0300

    Update runtime.exs

--- config/runtime.exs
+++ config/runtime.exs
@@ -39,14 +39,10 @@ if config_env() == :prod do
   config :razzor_dev, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :razzor_dev, RazzorDevWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    url: [host: System.get_env("PHX_HOST"), port: 443, scheme: "https"],
     http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
+      port: String.to_integer(System.get_env("PORT") || "4000")
     ],
     secret_key_base: secret_key_base
 

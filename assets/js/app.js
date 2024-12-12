0df814306563a7999ef33da353fdf5a43e761b90
razzor <adrianorazzor@gmail.com>
2024-12-12 20:47:53 -0300

    Implement dark mode toggle functionality; add event listener and update HTML structure for dark mode support

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,11 @@ import "phoenix_html"
 import { Socket } from "phoenix"
 import { LiveSocket } from "phoenix_live_view"
 import topbar from "../vendor/topbar"
+import initDarkModeToggle from "./components/dark_mode";
+
+document.addEventListener("DOMContentLoaded", () => {
+  initDarkModeToggle();
+});
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 

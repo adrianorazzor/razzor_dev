5990d7d8a8f991bd704fb369b39bdd65bdc6bfaf
razzor <adrianorazzor@gmail.com>
2024-12-12 22:25:38 -0300

    Refactor HTML structure; update navigation links and improve layout for better readability

--- lib/razzor_dev_web/live/home/home_live.ex
+++ lib/razzor_dev_web/live/home/home_live.ex
@@ -13,18 +13,18 @@ defmodule RazzorDevWeb.HomeLive do
   end
 
   def handle_event("change_locale", %{"locale" => locale}, socket) do
-  Gettext.put_locale(locale)
+    Gettext.put_locale(locale)
 
-  socket =
-    socket
-    |> assign(:locale, locale)
-    |> assign(:hello_text, gettext("hello"))
-    |> assign(:intro_text, gettext("intro_text"))
-    |> assign(:contact_text, gettext("contact_me"))
-    |> assign(:links_text, gettext("links"))
+    socket =
+      socket
+      |> assign(:locale, locale)
+      |> assign(:hello_text, gettext("hello"))
+      |> assign(:intro_text, gettext("intro_text"))
+      |> assign(:contact_text, gettext("contact_me"))
+      |> assign(:links_text, gettext("links"))
 
-  {:noreply, socket}
-end
+    {:noreply, socket}
+  end
 
   def render(assigns) do
     assigns = assign_new(assigns, :hello_text, fn -> gettext("hello") end)
@@ -32,59 +32,58 @@ end
     assigns = assign_new(assigns, :contact_text, fn -> gettext("contact_me") end)
     assigns = assign_new(assigns, :links_text, fn -> gettext("links") end)
 
-~H"""
-<div class="flex flex-col justify-center items-center min-h-screen text-center px-4">
-  <h1 class="text-4xl font-bold mb-4 animate-slide-in">{@hello_text}</h1>
-  <div
-    class="w-full max-w-2xl mx-auto"
-    id={"typewriter-#{@locale}"}
-    x-data={"{
-      text: '',
-      fullText: '#{Phoenix.HTML.javascript_escape(@intro_text)}',
-      isTyping: true,
-      typeWriter() {
-        this.text = '';
-        this.isTyping = true;
-        let i = 0;
-        const typeInterval = setInterval(() => {
-          if (i < this.fullText.length) {
-            this.text += this.fullText.charAt(i);
-            i++;
-          } else {
-            clearInterval(typeInterval);
-            this.isTyping = false;
+    ~H"""
+    <div class="flex flex-col justify-center items-center min-h-screen text-center px-4">
+      <h1 class="text-4xl font-bold mb-4 animate-slide-in">{@hello_text}</h1>
+      <div
+        class="w-full max-w-2xl mx-auto"
+        id={"typewriter-#{@locale}"}
+        x-data={"{
+          text: '',
+          fullText: '#{Phoenix.HTML.javascript_escape(@intro_text)}',
+          isTyping: true,
+          typeWriter() {
+            this.text = '';
+            this.isTyping = true;
+            let i = 0;
+            const typeInterval = setInterval(() => {
+              if (i < this.fullText.length) {
+                this.text += this.fullText.charAt(i);
+                i++;
+              } else {
+                clearInterval(typeInterval);
+                this.isTyping = false;
+              }
+            }, 50);
           }
-        }, 50);
-      }
-    }"}
-    x-init="typeWriter()"
-  >
-    <p class="text-xl mb-4">
-      <span x-text="text"></span>
-      <span
-        x-show="isTyping"
-        class="border-r-2 border-white animate-[blink_0.75s_step-end_infinite]"
+        }"}
+        x-init="typeWriter()"
       >
-      </span>
-    </p>
-  </div>
-  <a href="mailto:adrianorazzor@gmail.com" class="text-gray-400 hover:text-white underline">
-    {@contact_text}
-  </a>
-  <div class="flex space-x-4 mt-6 justify-center">
-    <a
-      href="https://www.linkedin.com/in/adriano-pereira-baungardt/"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <i class="fab fa-linkedin text-2xl"></i>
-    </a>
-    <a href="https://github.com/adrianorazzor" target="_blank" rel="noopener noreferrer">
-      <i class="fab fa-github text-2xl"></i>
-    </a>
-  </div>
-</div>
-"""
-
+        <p class="text-xl mb-4">
+          <span x-text="text"></span>
+          <span
+            x-show="isTyping"
+            class="border-r-2 border-white animate-[blink_0.75s_step-end_infinite]"
+          >
+          </span>
+        </p>
+      </div>
+      <a href="mailto:adrianorazzor@gmail.com" class="text-gray-400 hover:text-white underline">
+        {@contact_text}
+      </a>
+      <div class="flex space-x-4 mt-6 justify-center">
+        <a
+          href="https://www.linkedin.com/in/adriano-pereira-baungardt/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i class="fab fa-linkedin text-2xl"></i>
+        </a>
+        <a href="https://github.com/adrianorazzor" target="_blank" rel="noopener noreferrer">
+          <i class="fab fa-github text-2xl"></i>
+        </a>
+      </div>
+    </div>
+    """
   end
 end
